a69dbc9002cbd7a0dab970ea8f6312a698fda4c4
Svetlana Yakutina <s.yakutina@g.nsu.ru>
2024-09-07 03:06:48 +0700

    lab25

--- 22216/s.yakutina/lab25/lab25.c
+++ 22216/s.yakutina/lab25/lab25.c
@@ -7,7 +7,8 @@
 int main() {
     int fd[2]; 
     pid_t pid; 
-    char buffer[BUFSIZ]; 
+    char buffer_p[BUFSIZ];
+    char buffer_s[BUFSIZ]; 
 
     if (pipe(fd) == -1) { 
         perror("pipe");
@@ -23,15 +24,15 @@ int main() {
 
     else if (pid == 0) {
         close(fd[1]);
-        ssize_t count; 
+        ssize_t count_p; 
 
-        while ((count = read(fd[0], buffer, BUFSIZ)) > 0) { 
-            for (int i = 0; i < count; i++) {         
-                printf("%c", toupper(buffer[i]));
+        while ((count_p = read(fd[0], buffer_p, BUFSIZ)) > 0) { 
+            for (int i = 0; i < count_p; i++) {         
+                printf("%c", toupper(buffer_p[i]));
             }
         }
 
-        if (count == -1) {
+        if (count_p == -1) {
             perror("read");
             exit(1);
         }
@@ -42,9 +43,16 @@ int main() {
     else { 
         close(fd[0]);
 
-        char *text = "WwwwwwWWWW wwWwWw\n"; 
-        if (write(fd[1], text, strlen(text)) == -1) {
-            perror("write");
+        ssize_t count_s; 
+        while ((count_s = read(STDIN_FILENO, buffer_s, BUFSIZ)) > 0) {
+            if (write(fd[1], buffer_s, count_s) == -1) {
+                perror("write");
+                exit(1);
+            }
+        }
+
+        if (count_s == -1) {
+            perror("read");
             exit(1);
         }
 

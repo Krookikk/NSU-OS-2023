10b181f1a7e230268b2215c6f1d846de3d1dc292
Svetlana Yakutina <s.yakutina@g.nsu.ru>
2024-09-13 21:12:35 +0700

    lab31

---
+++ 22216/s.yakutina/lab31/client.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define BUFFER_SIZE 1024
+
+int main() {
+    char *sock_path = "./socket";
+    int client_fd, bytes_read;
+    struct sockaddr_un server_addr;
+    char buffer[BUFFER_SIZE];
+
+    if ((client_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, sock_path, sizeof(server_addr.sun_path) - 1);
+
+    if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        close(client_fd);
+        perror("connect");
+        exit(1);
+    }
+
+    while ((bytes_read = read(0, buffer, BUFFER_SIZE)) > 0) {
+        if (write(client_fd, buffer, bytes_read) == -1) {
+            close(client_fd);
+            perror("write");
+            exit(1);
+        }
+    }
+
+    close(client_fd);
+    return 0;
+}
---
+++ 22216/s.yakutina/lab31/server.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <poll.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 2048
+#define MAX_CLIENTS 10
+
+void sigcatch(int sig) {
+    unlink("./socket");
+    _exit(0);
+}
+
+int main() {
+    char *sock_path = "./socket";
+    int server_fd, client_fd, max_fd;
+    struct sockaddr_un server_addr;
+    struct pollfd poll_fds[MAX_CLIENTS + 1];
+    char buffer[BUFFER_SIZE];
+
+    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, sock_path, sizeof(server_addr.sun_path) - 1);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    signal(SIGINT, sigcatch);
+    signal(SIGQUIT, sigcatch);
+    
+    if (listen(server_fd, MAX_CLIENTS) == -1) {
+        unlink(sock_path);
+        perror("listen");
+        exit(1);
+    }
+
+    poll_fds[0].fd = server_fd;
+    poll_fds[0].events = POLLIN;
+
+    for (int i = 1; i <= MAX_CLIENTS; i++) {
+        poll_fds[i].fd = -1;
+    } 
+
+    while (1) {
+        int poll_count = poll(poll_fds, MAX_CLIENTS + 1, -1);
+
+        if (poll_count == -1) {
+            unlink(sock_path);
+            perror("poll");
+            exit(1);
+        }
+
+        if (poll_fds[0].revents & POLLIN) {
+            if ((client_fd = accept(server_fd, NULL, NULL)) == -1) {
+                unlink(sock_path);
+                perror("accept");
+                exit(1);
+            }
+
+            for (int i = 1; i <= MAX_CLIENTS; i++) {
+                if (poll_fds[i].fd == -1) {
+                    poll_fds[i].fd = client_fd;
+                    poll_fds[i].events = POLLIN;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 1; i <= MAX_CLIENTS; i++) {
+            if (poll_fds[i].revents & POLLIN) {
+                memset(buffer, 0, BUFFER_SIZE);
+                int bytes_read = read(poll_fds[i].fd, buffer, BUFFER_SIZE);
+
+                if (bytes_read > 0) {
+                    for (int j = 0; j < bytes_read; j++) {
+                        buffer[j] = toupper(buffer[j]);
+                        printf("%c", buffer[j]);
+                    }
+                } else {
+                    close(poll_fds[i].fd);
+                    poll_fds[i].fd = -1;
+                } 
+            }
+        }
+    }
+}
